bf2f6f0a871dc42c1b65399f25ac4c81aa1f4b13
sneli <emacsymic@gmail.com>
2025-03-13 13:37:08 -0600

    update test and jenkins file

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Eilidh's jenkins react app/i);
   expect(linkElement).toBeInTheDocument();
 });
